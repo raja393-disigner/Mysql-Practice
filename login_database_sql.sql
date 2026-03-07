f5203886d4d1476b81daf402bd64731db954dc3d
Raja Rautela <rajarautela393@gmail.com>
2026-03-07 21:39:27 +0530

    Add files via upload

---
+++ login_database_sql.sql
@@ -0,0 +1,12 @@
+use login_db;
+
+select*from  users;
+ insert into users(id , username ,password)values
+ (2,'raja','2342'),
+  (3,'deepak','2342'),
+   (4,'sumit','2342'),
+    (5,'manish','2342'),
+     (6,'lalit','2342'),
+      (7,'bhavesh','2342'),
+       (8,'neeraj','2342'),
+        (9,'sahil','2342');
